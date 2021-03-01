c917d160e71270d34ee4a2a2743452dfbc6f0701
念歌 <niange@qunhemail.com>
2021-03-01 14:16:46 +0800

    feat: add vanilla js, remove test file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "vstodo" is now active!');
   context.subscriptions.push(
     vscode.commands.registerCommand("vstodo.helloWorld", () => {
-      vscode.window.showInformationMessage("111 World from VSTodo!");
+      HelloWorldPanel.createOrShow(context.extensionUri)
     })
   );
 
