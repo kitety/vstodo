cc531f34420fea87438fe279dd0e75b80b6c7a5c
念歌 <niange@qunhemail.com>
2021-03-01 16:57:19 +0800

    feat: svelte basics 1

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,7 +1,29 @@
-<script lang="ts"></script>
+<script lang="ts">
+  let todos: { text: string; completed: boolean }[] = [];
+  let count = 0;
+  let text = "";
+</script>
 
 <div>Hello</div>
+<form>
+  <input type="text" bind:value={text} />
+  <pre>{JSON.stringify(todos,null,2)}</pre>
+</form>
 
+<!-- <input type="text" bind:value={text} />
+<button
+  on:click={() => {
+    count++;
+    console.log("count: ", count);
+  }}>increment {count}</button
+>
+<b>{text}</b>
+<button
+  on:click={() => {
+    text = "";
+  }}
+  >reset text
+</button> -->
 <style>
   div {
     color: red;
