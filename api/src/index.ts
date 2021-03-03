eec8a758470901200f013dabbe4f61898baa8e90
念歌 <niange@qunhemail.com>
2021-03-03 13:17:56 +0800

    refactor: directory refactor

---
+++ api/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const main = async () => {
+  const app = express();
+  app.get('/',(_req,res)=>{
+      res.send('hello')
+  })
+  app.listen(3002, () => {
+    console.log('The server is listening on port 3002!!');
+  });
+};
+main();
